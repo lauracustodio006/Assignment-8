96afe41be02beca3730a4f382ac0248d284c4832
laura <lcustodio@georgetowncollege.edu>
2025-11-04 12:10:41 -0600

    progress in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("script.js connected!");
+
+let questionBlock = document.querySelectorAll(".question-block");
+let userAnswers = {};
+
+questionBlock.forEach(function(block) {
+ let button = block.querySelectorAll(".answer-btn");
+
+    button.forEach(function(button){
+        button.addEventListener("click", function() {
+            button.forEach(function(btn){
+                btn.classList.remove("selected");
+            });
+
+        button.classList.add("selected");
+
+        let buttonID = button.dataset.buttonid;
+        let response = button.dataset.city;
+        userAnswers[buttonID] = response;
+
+         console.log(userAnswers);
+        });
+    });
+});
+let Zurich  = 0;
+let Rio = 0;
+let NY = 0;
+let Wellington = 0;
+for (let i in )
