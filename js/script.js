99e7a03db3fdcdfe1c34a031c162e1753343abd9
laura <lcustodio@georgetowncollege.edu>
2025-11-05 07:16:43 -0600

    final assignement

--- js/script.js
+++ js/script.js
@@ -1,29 +1,58 @@
 console.log("script.js connected!");
 
-let questionBlock = document.querySelectorAll(".question-block");
+let questionBlocks = document.querySelectorAll(".question-block");
 let userAnswers = {};
 
-questionBlock.forEach(function(block) {
- let button = block.querySelectorAll(".answer-btn");
-
-    button.forEach(function(button){
+questionBlocks.forEach(function(block, index) {
+    let buttons = block.querySelectorAll(".answer-btn"); 
+    buttons.forEach(function(button) {
         button.addEventListener("click", function() {
-            button.forEach(function(btn){
+            buttons.forEach(function(btn){
                 btn.classList.remove("selected");
             });
+            button.classList.add("selected"); 
+            userAnswers[index] = button.dataset.answer;
 
-        button.classList.add("selected");
-
-        let buttonID = button.dataset.buttonid;
-        let response = button.dataset.city;
-        userAnswers[buttonID] = response;
-
-         console.log(userAnswers);
+            console.log(userAnswers);
         });
     });
 });
-let Zurich  = 0;
-let Rio = 0;
-let NY = 0;
-let Wellington = 0;
-for (let i in )
+
+function displayResult() {
+    let Zurich = 0;
+    let Rio = 0;
+    let NY = 0;
+    let Wellington = 0;
+
+    for (let i in userAnswers) {
+        let city = userAnswers[i];
+        if (city == "Zurich, Switzerland") {
+            Zurich++;
+        } else if (city == "Rio de Janeiro, Brazil") {
+            Rio++;
+        } else if (city == "New York, U.S.A.") {
+            NY++;
+        } else if (city == "Wellington, New Zealand") {
+            Wellington++;
+        }
+    }
+
+    let finalCity = "";
+    if (Zurich >= Rio && Zurich >= NY && Zurich >= Wellington) {
+        finalCity = "Zurich, Switzerland";
+    } else if (Rio >= Zurich && Rio >= NY && Rio >= Wellington) {
+        finalCity = "Rio de Janeiro, Brazil";
+    } else if (NY >= Zurich && NY >= Rio && NY >= Wellington) {
+        finalCity = "New York, U.S.A.";
+    } else {
+        finalCity = "Wellington, New Zealand";
+    }
+
+     let resultContainer = document.querySelector("#result-container");
+    let resultText = document.querySelector("#result-text");
+    
+    resultContainer.style.display = "block";
+    resultText.textContent = finalCity;
+}
+
+document.querySelector("#show-result").addEventListener("click", displayResult);
